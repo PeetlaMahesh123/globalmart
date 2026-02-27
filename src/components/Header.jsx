63c05f47d3e5a4edce84835610d5ed5b1dfc69b8
peetlamahesh <peetlamahesh81@gmail.com>
2026-02-27 15:36:47 +0530

    Updated Header with logo and profile dropdown

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,20 +5,24 @@ import { ProfileDropdown } from "./ProfileDropdown";
 import "../styles/Header.css";
 
 export function Header({ cartCount, username }) {
+
   return (
     <header className="header">
+
       <div className="header-content">
-        
-        {/* Logo Component */}
+
         <Logo />
 
-        {/* Right Section */}
         <div className="header-actions">
+
           <CartIcon count={cartCount} />
+
           <ProfileDropdown username={username} />
+
         </div>
 
       </div>
+
     </header>
   );
 }
